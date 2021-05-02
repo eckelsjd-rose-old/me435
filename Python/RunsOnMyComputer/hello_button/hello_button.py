73e9a9fe7a40659935473a563ec357dca0a3fbb8
Joshua Eckels <eckelsjd@rose-hulman.edu>
2021-05-02 12:35:45 -0400

    local hello button

--- Python/RunsOnMyComputer/hello_button/hello_button.py
+++ Python/RunsOnMyComputer/hello_button/hello_button.py
@@ -1,7 +1,25 @@
 from kivymd.app import MDApp
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 class HelloButtonApp(MDApp):
+    
+    counter_text = StringProperty("Counter = 0")
+
+    def __init__(self,**kwargs):
+        super(HelloButtonApp, self).__init__(**kwargs)
+        self.counter = 0
+
+    def set_counter(self, value):
+        self.counter = value
+        self.update_view()
+
+    def change_counter(self, value):
+        self.counter += value
+        self.update_view()
+
+    def update_view(self):
+        self.counter_text = f'Counter = {self.counter}'
 
     def build(self):
         # Done in magic .kv file HelloButtonApp --> hellobutton.kv
