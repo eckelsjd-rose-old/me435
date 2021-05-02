dcfe86352a3fbd55054e34300b0b1ac73aba6513
Joshua Eckels <eckelsjd@rose-hulman.edu>
2021-05-02 13:18:30 -0400

    mqtt part 1

--- Python/RunsOnMyComputer/hello_button/hello_button_mqtt.py
+++ Python/RunsOnMyComputer/hello_button/hello_button_mqtt.py
@@ -11,14 +11,29 @@ class HelloButtonApp(MDApp):
         super(HelloButtonApp, self).__init__(**kwargs)
         self.counter = 0
         self.mqtt_client = mqtt_helper.MqttClient()
+        self.mqtt_client.callback = lambda message_type,payload: self.mqtt_callback(message_type,payload)
+        self.mqtt_client.connect(subscription_topic_name="eckelsjd", publish_topic_name="eckelsjd")
+
+    def mqtt_callback(self,message_type,payload):
+        print("MQTT messsage_type", message_type)
+        print("MQTT payload", payload)
+
+        if message_type == "set":
+            self.counter = payload
+            self.update_view()
+        elif message_type == "change":
+            self.counter += payload
+            self.update_view()
 
     def set_counter(self, value):
-        self.counter = value
-        self.update_view()
+        # self.counter = value
+        # self.update_view()
+        self.mqtt_client.send_message("set",value)
 
     def change_counter(self, value):
-        self.counter += value
-        self.update_view()
+        # self.counter += value
+        # self.update_view()
+        self.mqtt_client.send_message("change",value)
 
     def update_view(self):
         self.counter_text = f'Counter = {self.counter}'
